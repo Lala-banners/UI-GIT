ae156bd6fe9b537c5eadd7fd36b4fb7980dfa0ef
Lala-banners <lala.l.banno@hotmail.com>
2020-09-11 16:55:53 +1000

    Second commit

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,36 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-
     public float speed = 6f;
-
-    //Controls speed of rotations
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.0f;
+    public Vector3 playerVelocity;
     public float turnSmoothTime = 0.1f;
-    public float turnSmoothVelocity;
-
-    private void Update()
+    float turnSmoothVelocity;
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Movement();
+    }
+    private void Movement()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-
-        if (direction.magnitude >=0.1f)
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            //Smooth damp angle gradually changes an angle given in degrees toward a desired goal
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f); //Getting the angle we need and setting rotation to that angle
-
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg
+                                                                        + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle,
+                                            ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime);
         }
-
-
+    }
+    void Jump()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
